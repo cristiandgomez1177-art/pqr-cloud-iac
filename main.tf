5092852f41f61e11b7d48dd7003cf68f0e2302d0
cristiandgomez1177-art <cristiandgomez1177@gmail.com>
2026-07-03 22:39:43 +0000

    test

--- main.tf
+++ main.tf
@@ -37,10 +37,10 @@ module "apigateway" {
 }
 
 # S3
-module "s3" {
-  source      = "./modules/s3"
-  bucket_name = var.s3_bucket_name
-}
+# module "s3" {
+#  source      = "./modules/s3"
+#  bucket_name = var.s3_bucket_name
+#}
 
 # SQS
 module "sqs" {
